9babea0d35ce83790613da09c926a7dae0f4bce4
Toilal <toilal.dev@gmail.com>
2015-03-09 11:16:56 +0100

    [SSL] Cleanup SSL certificate generation

--- my_init.d/10-ssl.sh
+++ my_init.d/10-ssl.sh
@@ -38,9 +38,7 @@ if [[ ! -f /home/box/ssl/$DOMAIN.key || ! -f /home/box/ssl/$DOMAIN.crt ]]; then
   export PASSPHRASE=$(head -c 500 /dev/urandom | tr -dc a-z0-9A-Z | head -c 128; echo)
 
   # Certificate details; replace items in angle brackets with your own info
-  subj="/O=stealth-box"
-
-  echo subj
+  subj="/O=$DOMAIN"
 
   # Generate the server private key
   openssl genrsa -des3 -out $DOMAIN.key -passout env:PASSPHRASE 2048
