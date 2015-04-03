db29cd2983d027e48baf1807cb6c4a2fb56763dd
Toilal <toilal.dev@gmail.com>
2015-04-03 14:27:35 +0200

    Create SSH configuration directory if not exists

--- my_init.d/000_before_regen_ssh_host_keys.sh
+++ my_init.d/000_before_regen_ssh_host_keys.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 if [[ ! -e /etc/service/sshd/down && ! -e /home/box/etc/ssh/sshd_config ]] || [[ "$1" == "-f" ]]; then
+    mkdir -p /home/box/etc/ssh/
     cp -R /etc/ssh.default/* /home/box/etc/ssh/
 fi
