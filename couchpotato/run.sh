cdeec8fb3dd64d6851226f0ad9b51ab95a015baf
Toilal <toilal.dev@gmail.com>
2016-10-18 23:47:22 +0200

    Add password customization in couchpotato
    Customization is performed through stealthbox-conf data container.
    
    stealthbox-conf data container contains a single stealthbox.json file.
    
    This configuration file is read when couchpotato is starting, so couchpotato application configuration can be updated before running couchpotato.

---
+++ couchpotato/run.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+cat data/settings.conf>run.sh.tmp
+
+echo -e "[core]">>run.sh.tmp
+echo -e "password = $(jq -r "if .couchpotato.password then .couchpotato.password else .password end" ../conf/stealthbox.json | xargs echo -n | md5sum | awk '{print $1}')">>run.sh.tmp
+
+echo -e "">>run.sh.tmp
+echo -e "[deluge]">>run.sh.tmp
+echo -e "password = $(jq -r "if .deluge.password then .deluge.password else .password end" ../conf/stealthbox.json | xargs echo -n)">>run.sh.tmp
+cat run.sh.tmp | ../tools/ConfigParserPipe.py> data/settings.conf
+rm run.sh.tmp
+
+python install/CouchPotato.py --data_dir data
