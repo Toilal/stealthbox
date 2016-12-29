b854da849ae489fa7f81d47cefdfd82c184520c8
Toilal <toilal.dev@gmail.com>
2016-12-29 15:19:49 +0100

    Add password customization in headphones

---
+++ headphones/run.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+cat data/config.ini>run.sh.tmp
+
+echo -e "[General]">>run.sh.tmp
+echo -e "http_password = $(jq -r "if .headphones.password then .headphones.password else .password end" ../conf/stealthbox.json | xargs echo -n | awk '{print $1}')">>run.sh.tmp
+
+echo -e "">>run.sh.tmp
+echo -e "[Deluge]">>run.sh.tmp
+echo -e "deluge_password = $(jq -r "if .deluge.password then .deluge.password else .password end" ../conf/stealthbox.json | xargs echo -n)">>run.sh.tmp
+cat run.sh.tmp | ../tools/ConfigParserPipe.py> data/config.ini
+rm run.sh.tmp
+
+python install/Headphones.py -p 8181 --datadir data --nolaunch
